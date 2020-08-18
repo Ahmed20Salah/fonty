f9c8a4a34d985116a31226734779118f572ccff6
Ahmed Salah <Ahmedsalah2820@gmail.com>
2020-08-18 14:24:03 +0200

    notification v1

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,10 +1,10 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fonty/bloc/user_bloc/user_bloc.dart';
 import 'package:fonty/pages/forget_password.dart';
 import 'package:fonty/pages/home.dart';
 import 'package:fonty/pages/register.dart';
+import 'package:fonty/utilits/constant.dart';
 
 class LoginScreen extends StatelessWidget {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
@@ -14,106 +14,170 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Form(
-          key: _formKey,
-          child: Column(
+      body: Stack(
+        children: <Widget>[
+           Column(
             children: <Widget>[
-              SizedBox(
-                height: 24.0,
-              ),
               Container(
-                height: 50.0,
-                color: Colors.black,
-                width: MediaQuery.of(context).size.width,
-                alignment: Alignment.center,
-                child: Text(
-                  'تسجيل الدخول',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-              SizedBox(
-                height: 50.0,
-              ),
-              _emailfield(),
-              _passwordfield(),
-              SizedBox(
-                height: 70.0,
-              ),
-              _submitButton(),
-              SizedBox(
-                height: 40.0,
-              ),
-              Center(
-                child: InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => ForgetPasswordScreen()));
-                  },
-                  child: Text(' نسيت كلمة المرور؟', style: _titleStyle()),
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Center(
-                child: InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => RegisterScrren()));
-                  },
-                  child: Text(
-                    ' عضو جديد ؟ سجل الان',
-                    style: _titleStyle(),
-                  ),
-                ),
-              ),
-              BlocListener<UserBloc, UserState>(
-                  bloc: _bloc,
-                  listener: (context, state) {
-                    if (state is Loading) {
-                      showDialog(
-                        context: context,
-                        child: AlertDialog(
-                          elevation: 0.0,
-                          backgroundColor: Colors.transparent,
-                          content: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              CircularProgressIndicator(),
-                            ],
-                          ),
-                        ),
-                      );
-                    } else if (state is Error) {
-                      Navigator.pop(context);
-                      Scaffold.of(context).showSnackBar(
-                        SnackBar(
-                          content: Container(
-                            height: 20.0,
-                            alignment: Alignment.center,
-                            child: Text(state.error),
-                          ),
-                        ),
-                      );
-                    } else if (state is Authenticated) {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => Home()),
-                      );
-                    }
-                  },
-                  child: Container())
+                height: 210,
+                width: MediaQuery.of(context).size.height,
+                color: ConstantData().red,
+              )
             ],
           ),
-        ),
+          SingleChildScrollView(
+            child: Container(
+              margin: EdgeInsets.only(top: 125.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            height: 140.0,
+                            margin: EdgeInsets.symmetric(horizontal: 80.0),
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage('assets/sandwitch.png'),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 120.0),
+                            child: Text(
+                              'مرحبا بك …',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                          Center(
+                            child: Text(
+                              'تسجيل الدخول',
+                              style: TextStyle(
+                                  color: ConstantData().red,
+                                  fontSize: 27,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    input(
+                        img: 'assets/email.png',
+                        con: _email,
+                        val: (val) {
+                          if (val.isEmpty || !val.contains('@')) {
+                            return 'يرجي ادخال ايميل صحيح';
+                          }
+                        },
+                        title: 'البريد الالكتروني'),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    input(
+                        img: 'assets/lock.png',
+                        con: _password,
+                        password: true,
+                        val: (val) {
+                          if (val.isEmpty) {
+                            return 'يرجي ادخال كلمه سر صحيح';
+                          }
+                        },
+                        title: 'كلمة المرور'),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10.0),
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      ForgetPasswordScreen()));
+                        },
+                        child: Text(' نسيت كلمة المرور؟', style: _titleStyle()),
+                      ),
+                    ),
+                    SizedBox(height: 40.0),
+                    _submitButton(),
+                    SizedBox(
+                      height: 25.0,
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 20.0),
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => RegisterScrren()));
+                        },
+                        child: Container(
+                          alignment: Alignment.center,
+                          height: 53,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              color: ConstantData().black),
+                          child: Text(
+                            'تسجيل حساب جديد',
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                    BlocListener<UserBloc, UserState>(
+                        bloc: _bloc,
+                        listener: (context, state) {
+                          if (state is Loading) {
+                            showDialog(
+                              context: context,
+                              child: AlertDialog(
+                                elevation: 0.0,
+                                backgroundColor: Colors.transparent,
+                                content: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    CircularProgressIndicator(),
+                                  ],
+                                ),
+                              ),
+                            );
+                          } else if (state is Error) {
+                            Navigator.pop(context);
+                            Scaffold.of(context).showSnackBar(
+                              SnackBar(
+                                content: Container(
+                                  height: 20.0,
+                                  alignment: Alignment.center,
+                                  child: Text(state.error),
+                                ),
+                              ),
+                            );
+                          } else if (state is Authenticated) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => Home()),
+                            );
+                          }
+                        },
+                        child: Container())
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -136,54 +200,31 @@ class LoginScreen extends StatelessWidget {
               },
               child: Container(
                 alignment: Alignment.center,
-                height: 45,
+                height: 53,
                 decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5),
-                    color: Color(0xffFABF18)),
-                child: Text(
-                  'تسجيل الدخول',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    borderRadius: BorderRadius.circular(15),
+                    color: ConstantData().yellow),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Text(
+                      'تسجيل الدخول',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      width: 80.0,
+                    ),
+                    Icon(Icons.arrow_forward),
+                    SizedBox(
+                      width: 40.0,
+                    ),
+                  ],
                 ),
               ),
             ),
           );
         });
-  }
-
-  Container _emailfield() {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 20.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          SizedBox(
-            height: 10.0,
-          ),
-          Text(
-            'البريد الالكتروني',
-            style: _titleStyle(),
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          TextFormField(
-            validator: (val) {
-              if (val.isEmpty || !val.contains('@')) {
-                return 'يرجي ادخال ايميل صحيح';
-              }
-            },
-            controller: _email,
-            decoration: InputDecoration(
-              focusedBorder: _inputBorder(),
-              enabledBorder: _inputBorder(),
-              contentPadding:
-                  EdgeInsets.symmetric(vertical: 5, horizontal: 10.0),
-              border: _inputBorder(),
-            ),
-          )
-        ],
-      ),
-    );
   }
 
   Container _passwordfield() {
@@ -204,7 +245,6 @@ class LoginScreen extends StatelessWidget {
           ),
           TextFormField(
             obscureText: true,
-
             validator: (val) {
               if (val.isEmpty) {
                 return 'يرجي ادخال كلمه مرور ';
@@ -212,7 +252,10 @@ class LoginScreen extends StatelessWidget {
             },
             controller: _password,
             decoration: InputDecoration(
-              focusedBorder: _inputBorder(),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: BorderSide(color: Colors.red),
+              ),
               enabledBorder: _inputBorder(),
               contentPadding:
                   EdgeInsets.symmetric(vertical: 5, horizontal: 10.0),
@@ -225,14 +268,82 @@ class LoginScreen extends StatelessWidget {
   }
 
   TextStyle _titleStyle() => TextStyle(
-        fontSize: 15,
+        fontSize: 16,
         fontWeight: FontWeight.w600,
       );
-
-  OutlineInputBorder _inputBorder() {
-    return OutlineInputBorder(
-      borderRadius: BorderRadius.circular(5),
-      borderSide: BorderSide(color: Colors.black),
-    );
-  }
 }
+
+Widget input(
+    {String title,
+    bool password = false,
+    TextEditingController con,
+    String img,
+    Function val}) {
+  return Container(
+    margin: EdgeInsets.symmetric(horizontal: 5.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(
+          height: 10.0,
+        ),
+        Row(
+          children: <Widget>[
+            Container(
+              width: 7,
+              height: 7,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle, color: ConstantData().red),
+            ),
+            SizedBox(
+              width: 10.0,
+            ),
+            Text(
+              title,
+              style: _titleStyle(),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: TextFormField(
+            validator: val,
+            controller: con,
+            obscureText: password,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: ConstantData().gray,
+              prefixIcon: Image.asset(
+                img,
+                height: 20.0,
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: BorderSide(color: Colors.red),
+              ),
+              enabledBorder: _inputBorder(),
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 5, horizontal: 10.0),
+              border: _inputBorder(),
+            ),
+          ),
+        )
+      ],
+    ),
+  );
+}
+
+OutlineInputBorder _inputBorder() {
+  return OutlineInputBorder(
+    borderRadius: BorderRadius.circular(8),
+    borderSide: BorderSide(color: Colors.black),
+  );
+}
+
+TextStyle _titleStyle() => TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w600,
+    );
